ccd4d2cbe5e240fa1432f68a677a54fe486a850c
Maria <bondaryikmaria@gmail.com>
2024-02-17 19:13:12 +0300

    create string

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+
+    {
+        // Пример строки для преобразования в нижний регистр
+        string input = "Hello, World!";
+
+      
+
